b1b09ea07bed42fde88198d05be571f8f3860df7
Chirag Dogra <cd.chiragdogra@gmail.com>
2025-07-20 21:58:48 +0530

    adding initial files for login app

---
+++ login-app/server/src/index.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import express from 'express'
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+(async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URI}/${process.env.DB_NAME}`);
+    console.log("MongoDB connected successfully. DB Host:", connectionInstance.connection.host, "DB Name:", connectionInstance.connection.name);
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+})();
+
+app.get('/', (req, res) => {
+    res.send('server home page!');
+});
