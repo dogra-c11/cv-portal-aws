50e71b81173c5e5b211fe375b7f8a121cd79a3f5
Chirag Dogra <cd.chiragdogra@gmail.com>
2025-07-22 19:54:31 +0530

    rate limit for login api

--- login-app/server/src/routes/auth.js
+++ login-app/server/src/routes/auth.js
@@ -1,11 +1,20 @@
 import express from 'express';
 import cors from 'cors';
+import { rateLimit } from 'express-rate-limit'
 import { registerUser, loginUser, getUser } from '../controllers/authController.js';
 import { validJson } from '../middlewares/validJson.js';
 const router = express.Router()
+const rateLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 5, 
+	standardHeaders: 'draft-8',
+    message: {'error' : 'Too many login attempts. Please try again after some time.'},
+	legacyHeaders: false, 
+	ipv6Subnet: 56, 
+})
 router.use(cors());
 router.use(validJson);
 router.post('/register', registerUser);
-router.post('/login', loginUser);
+router.post('/login', rateLimiter, loginUser);
 router.get('/user', getUser);
 export default router;
